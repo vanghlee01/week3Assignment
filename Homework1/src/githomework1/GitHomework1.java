f7ab9c1f847c9d2593de59d1b8ee0133e9085b33
Hlee Vang <vanghlee01@gmail.com>
2023-11-18 18:19:47 -0600

    add githomework1

---
+++ Homework1/src/githomework1/GitHomework1.java
@@ -0,0 +1,12 @@
+package githomework1;
+
+public class GitHomework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+while(true) {
+	System.out.println("a");
+	}
+
+}
+}
